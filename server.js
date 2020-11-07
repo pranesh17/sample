cf9ad1b5d8131919e52a674ad5b506e5e25c8558
pranesh muthu <praneshmj17.com>
2020-11-07 19:51:35 +0530

    third commit

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ app.use(express.static("public"));
 app.get("/", (req,res)=>{
     res.render("landing");
   });
-
+  app.get("/login", (req,res)=>{
+    res.render("login");
+  });
+  
+  app.get("/register", (req,res)=>{
+    res.render("register");
+  });
   const port=process.env.port || 8080;
   app.listen(port, function() {
     console.log("Server started on port 8080");
