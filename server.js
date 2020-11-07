1292a07aaf1a6bc9fa79a201ee05051ac856b943
pranesh muthu <praneshmj17.com>
2020-11-07 19:23:39 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const ejs = require("ejs");
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.get("/", (req,res)=>{
+    res.render("landing");
+  });
+
+  const port=process.env.port || 3000;
+app.listen(port, function() {
+  console.log("Server started on port 3000");
+});
