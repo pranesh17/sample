205f99c8771151efdafd8db0170aaf7b63471f68
pranesh muthu <praneshmj17.com>
2020-11-08 19:23:55 +0530

    removed awss3

--- server.js
+++ server.js
@@ -1,29 +1,16 @@
 const express = require("express");
 const ejs = require("ejs");
 const app = express();
-const AWS = require('aws-sdk');
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
-const s3 = new AWS.S3( );
 
 
 app.get("/", (req,res)=>{
     res.render("landing");
   });
 
-
   app.get("/login", (req,res)=>{
     res.render("login");
-    var s3bucket = new AWS.S3();
-    var params = {Bucket: 'dailyjournal123', Key:'sampletextnew.txt',
-    Body: 'Hello this data!'};
-    s3bucket.putObject(params, function(err, data) {
-    if (err) {
-     console.log("Error uploading data: ", err);
-     } else {
-      console.log("Successfully uploaded data to bucket");
-     }
-     });
   });
   
   app.get("/register", (req,res)=>{
